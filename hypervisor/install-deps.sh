4b5db87db41c39705ce9ddc6c9ec7495c334cb7b
oscar111 <yujin609@163.com>
2014-11-02 10:26:25 -0500

    add check for the result of running curl.

--- hypervisor/install-deps.sh
+++ hypervisor/install-deps.sh
@@ -9,26 +9,26 @@ if [ ! -f /etc/centos-release ];then
 fi
 
 # prepare repo for OS
-curl -O http://www.wiaapp.cn/downloads/litevirt/repo/yum.repos.d/CentOS-163.repo 
-if [ ! -f CentOS-163.repo ];then
-  echo "download repo file failed"
-  exit 1
-fi
-mv CentOS-163.repo /etc/yum.repos.d/CentOS-163.repo
+mkdir -p /tmp/yum.repos.d
+mv -f /etc/yum.repos.d/* /tmp/yum.repos.d/
 
-curl -O http://www.wiaapp.cn/downloads/litevirt/repo/yum.repos.d/epel.repo
-if [ ! -f epel.repo ];then
-  echo "download repo file failed"
-  exit 1
-fi
-mv epel.repo /etc/yum.repos.d/epel.repo
+repos="CentOS-163.repo epel.repo litevirt-hypervisor.repo"
+remote_repo_path="http://www.wiaapp.cn/downloads/litevirt/repo/yum.repos.d/"
+for repo in $repos
+do
+  rst=`curl -s -o /dev/null -w "%{http_code}" $remote_repo_path$repo` 
+  if [ $rst -ne 200 ];then
+    echo "download repo $remote_repo_path$repo failed"
+    exit 1
+  fi
 
-curl -O http://www.wiaapp.cn/downloads/litevirt/repo/yum.repos.d/litevirt-hypervisor.repo
-if [ ! -f litevirt-hypervisor.repo ];then
-  echo "download repo file failed"
-  exit 1
-fi
-mv litevirt-hypervisor.repo /etc/yum.repos.d/litevirt-hypervisor.repo
+  curl -O $remote_repo_path$repo
+  if [ ! -f $repo ];then
+    echo "download repo $remote_repo_path$repo failed"
+    exit 1
+  fi
+  mv $repo /etc/yum.repos.d/
+done
 
 # install dependent packages for building iso.
 pkgs="gcc make autoconf automake gettext-devel git python-cherrypy python-cheetah libxml2-python python-imaging \
@@ -36,4 +36,3 @@ PyPAM m2crypto python-jsonschema rpm-build python-psutil python-ethtool sos pyth
 iscsi-initiator-utils libxslt pyparted nginx python-unittest2 python-ordereddict hardlink pykickstart createrepo \
 livecd-tools appliance-tools appliance-tools-minimizer policycoreutils-python selinux-policy-devel grub2-efi"
 yum install -y $pkgs
-
