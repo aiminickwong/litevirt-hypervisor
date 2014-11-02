426e0c1aa2f71005fc3074fda1759d0057360dd5
oscar111 <yujin609@163.com>
2014-11-02 11:46:35 +0800

    update repo position.

--- hypervisor/install-deps.sh
+++ hypervisor/install-deps.sh
@@ -10,21 +10,21 @@ if [ ! -f /etc/centos-release ];then
 fi
 
 # prepare repo for OS
-wget http://www.wiaapp.cn/downloads/litevirt/repo/hypervisor/CentOS-163.repo 
+curl -O http://www.wiaapp.cn/downloads/litevirt/repo/yum.repos.d/CentOS-163.repo 
 if [ ! -f CentOS-163.repo ];then
   echo "download repo file failed"
   exit 1
 fi
 mv CentOS-163.repo /etc/yum.repos.d/CentOS-163.repo
 
-wget http://www.wiaapp.cn/downloads/litevirt/repo/hypervisor/epel.repo
+curl -O http://www.wiaapp.cn/downloads/litevirt/repo/yum.repos.d/epel.repo
 if [ ! -f epel.repo ];then
   echo "download repo file failed"
   exit 1
 fi
 mv epel.repo /etc/yum.repos.d/epel.repo
 
-wget http://www.wiaapp.cn/downloads/litevirt/repo/hypervisor/litevirt-hypervisor.repo
+curl -O http://www.wiaapp.cn/downloads/litevirt/repo/yum.repos.d/litevirt-hypervisor.repo
 if [ ! -f litevirt-hypervisor.repo ];then
   echo "download repo file failed"
   exit 1
@@ -35,7 +35,7 @@ mv litevirt-hypervisor.repo /etc/yum.repos.d/litevirt-hypervisor.repo
 pkgs="gcc make autoconf automake gettext-devel git python-cherrypy python-cheetah libxml2-python python-imaging \
 PyPAM m2crypto python-jsonschema rpm-build python-psutil python-ethtool sos python-ipaddr python-lxml nfs-utils \
 iscsi-initiator-utils libxslt pyparted nginx python-unittest2 python-ordereddict hardlink pykickstart createrepo \
-livecd-tools appliance-tools appliance-tools-minimizer policycoreutils-python selinux-policy-devel,grub2-efi"
+livecd-tools appliance-tools appliance-tools-minimizer policycoreutils-python selinux-policy-devel grub2-efi"
 #yum install -y $pkgs
 for pkg in $pkgs
 do
